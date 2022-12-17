10da16eaf200265546ab898ed7a43586f8235925
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-12-16 23:24:29 -0300

    uses map and filter

--- array-cardio-day-01/script.js
+++ array-cardio-day-01/script.js
@@ -76,3 +76,10 @@ const oldest = inventors.sort(function(a,b){
 });
 
 console.table(oldest);
+
+const category = document.querySelector('mw-category');
+const links = Array.from(category.querySelectorAll(a));
+const de = links
+            .map(link => link.textContent)
+            .filter(streetName => streetName.includes('de'));
+console.log(de);
