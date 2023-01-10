db0a335767eef158b42a6d9abbb02838b91dea6f
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-10 13:39:09 -0300

    using reduce to sum up the itens in an array

--- array-cardio-day-01/script.js
+++ array-cardio-day-01/script.js
@@ -97,3 +97,18 @@ const alpha = people.sort((lastOne, nextOne)=>{
 });
 
 console.log(alpha);
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+ const data = ['car', 'car', 'truck', 'truck', 'bike',
+  'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'pogostick'];
+
+const transportation = data.reduce(function(obj, item){
+if (!obj[item]){
+  obj[item]= 0;
+}
+obj[item]++;
+return obj;
+},{});
+
+console.log(transportation);
