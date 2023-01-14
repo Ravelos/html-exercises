3645a0187cd71766e45651a913314494ed3bdb22
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-13 22:28:37 -0300

    uses findIndex to delete an element

--- array-cardio-2/script.js
+++ array-cardio-2/script.js
@@ -49,3 +49,14 @@ console.log(comment);
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+
+const index = comments.findIndex(comment => comment.id === 823423)
+
+console.log(index);
+
+// comments.splice(index, 1);
+
+const newComments = [
+    ...comments.slice(0,index),
+    ...comments.slice(index + 1)
+]
