789c57cd9f0662235851b7ec48e1e5ba1e18cdcf
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-21 12:48:11 -0300

    grabs the mouse move and links it to the progress bar

--- Custom-Html5-Video-Player/script.js
+++ Custom-Html5-Video-Player/script.js
@@ -38,6 +38,12 @@ function handleProgress() {
     progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e)
+}
+
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -50,3 +56,8 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
+let mousedown = false;
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
