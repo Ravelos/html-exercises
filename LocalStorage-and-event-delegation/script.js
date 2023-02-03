97f6c950d78ccdb8fe7538fed86d5f99d4f7ad16
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-02 23:05:38 -0300

    creates the script file

---
+++ LocalStorage-and-event-delegation/script.js
@@ -0,0 +1,31 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = JSON.parse(localStorage.getItem(items)) || [];
+
+function addItem(e){
+  e.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+      text,
+      done: false
+  };
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+  this.reset();
+}
+
+function populateList(plates = [] , platesList){
+  platesList.innerHTML = plates.map((plate, i) =>{
+      return `
+      <li>
+          <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}/>
+          <label for="item${i}">${plate.text}</label>   
+      </li>
+      `;
+  }).join('');
+}
+
+addItems.addEventListener('submit', addItem);
+
+populateList(items. itemList);
