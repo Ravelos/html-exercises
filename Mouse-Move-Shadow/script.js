a7811836b5c3dab8f8d845d82409ec3d68fcb79c
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-12 17:17:34 -0300

    changes the px for walk const

--- Mouse-Move-Shadow/script.js
+++ Mouse-Move-Shadow/script.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
-const walk = 100 // 100px
+const walk = 500 // 100px
 
 function shadow(e){
     const { offsetWidth: width, offsetHeight: height } = hero;
