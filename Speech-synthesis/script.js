f169123b282fa722cee041921103bb578c5629a1
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-08 18:27:30 -0300

    works on the functionality to choos the voice and the voices dropdown

--- Speech-synthesis/script.js
+++ Speech-synthesis/script.js
@@ -2,5 +2,24 @@ const msg = new SpeechSynthesisUtterance();
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector('#speak');
-const stopButton = document.querySelector('#stop');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
+
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+
+  voicesDropdown.innerHTML = voices.map(
+    (voice) =>
+      `<option value="${voice.name}>${voice.name} (${voice.lang})</option>`
+  ).join('');
+}
+
+function setVoice(){
+    msg.voice = voices.find(voice => voice.name === this.value);
+    console.log(this.value)
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
