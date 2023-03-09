3fd9e0be0bc21d88c2dcc624822e67f00e1cda8b
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-09 16:14:37 -0300

    adds functionality to speak button

--- Speech-synthesis/script.js
+++ Speech-synthesis/script.js
@@ -29,5 +29,14 @@ function toggle(startOver = true) {
   }
 }
 
+function setOption(){
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
+
 speechSynthesis.addEventListener("voiceschanged", populateVoices);
 voicesDropdown.addEventListener("change", setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
