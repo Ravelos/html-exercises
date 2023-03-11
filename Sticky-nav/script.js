1e566dd4e92538c9850c026e2bbaaf2eecd8fe44
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-10 22:34:32 -0300

    adds padding top style

--- Sticky-nav/script.js
+++ Sticky-nav/script.js
@@ -4,8 +4,10 @@ const topOfNav = nav.offsetTop
 
 function fixNav(){
     if(window.scrollY >= topOfNav){
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
         document.body.classList.add('fixed-nav');
     }else{
+        document.body.style.paddingTop = 0;
         document.body.classList.remove('fixed-nav');
     }
 }
