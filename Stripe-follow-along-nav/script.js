3f5684f7565294c55a0ad5a4f9180156b2f52390
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-16 22:26:24 -0300

    Adds the class open to the background

--- Stripe-follow-along-nav/script.js
+++ Stripe-follow-along-nav/script.js
@@ -6,10 +6,12 @@ const nav = document.querySelector('.top');
 function handleEnter(){
     this.classList.add('trigger-enter');
     setTimeout(()=> this.classList.add('trigger-enter-active'), 150)
+    background.classList.add('open');
 }
 
 function handleLeave(){
     this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
 
 }
 
