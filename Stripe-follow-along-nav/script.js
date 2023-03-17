34fa0e71fae887a2b33e54976964efc5b6e2ec0b
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-16 22:06:21 -0300

    creates the functions handleEnter and HandleLeave

--- Stripe-follow-along-nav/script.js
+++ Stripe-follow-along-nav/script.js
@@ -1,3 +1,16 @@
 const triggers = document.querySelectorAll('.cool > li');
 const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
+
+
+function handleEnter(){
+console.log('ENTER!!!')
+}
+
+function handleLeave(){
+    console.log('LEAVE!!!')
+
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
