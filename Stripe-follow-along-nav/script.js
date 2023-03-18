ce6cb511d380395fb51f21f19f552056ed0f9eb0
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-17 21:49:59 -0300

    Refactor setTimeout function

--- Stripe-follow-along-nav/script.js
+++ Stripe-follow-along-nav/script.js
@@ -4,23 +4,39 @@ const nav = document.querySelector(".top");
 
 function handleEnter() {
   this.classList.add("trigger-enter");
-  setTimeout(() => this.classList.add("trigger-enter-active"), 150);
+  setTimeout(() => {
+    if (this.classList.contains("trigger-enter")) {
+      this.classList.add("trigger-enter-active");
+    }
+  }, 150);
+
+  /* There is another way to do this function
+  setTimeout(
+    () =>
+      this.classList.contains("trigger-enter") &&
+      this.classList.add("trigger-enter-active"),
+    150
+  ); */
+
   background.classList.add("open");
 
   const dropdown = this.querySelector(".dropdown");
   const dropdownCoords = dropdown.getBoundingClientRect();
   const navCoords = nav.getBoundingClientRect();
-  
+
   const coords = {
     height: dropdownCoords.height,
     width: dropdownCoords.width,
     top: dropdownCoords.top - navCoords.top,
-    left: dropdownCoords.left - navCoords.left
+    left: dropdownCoords.left - navCoords.left,
   };
 
-  background.style.setProperty('height', `${coords.height}px`);
-  background.style.setProperty('width', `${coords.width}px`);
-  background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+  background.style.setProperty("height", `${coords.height}px`);
+  background.style.setProperty("width", `${coords.width}px`);
+  background.style.setProperty(
+    "transform",
+    `translate(${coords.left}px, ${coords.top}px)`
+  );
 }
 
 function handleLeave() {
