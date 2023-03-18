d5ecc0719fe032e96a9b569e59918f765abed6f6
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-17 21:16:12 -0300

    Gets dropdown coords

--- Stripe-follow-along-nav/script.js
+++ Stripe-follow-along-nav/script.js
@@ -1,19 +1,25 @@
-const triggers = document.querySelectorAll('.cool > li');
-const background = document.querySelector('.dropdownBackground');
-const nav = document.querySelector('.top');
+const triggers = document.querySelectorAll(".cool > li");
+const background = document.querySelector(".dropdownBackground");
+const nav = document.querySelector(".top");
 
+function handleEnter() {
+  this.classList.add("trigger-enter");
+  setTimeout(() => this.classList.add("trigger-enter-active"), 150);
+  background.classList.add("open");
 
-function handleEnter(){
-    this.classList.add('trigger-enter');
-    setTimeout(()=> this.classList.add('trigger-enter-active'), 150)
-    background.classList.add('open');
+  const dropdown = this.querySelector(".dropdown");
+  const dropdownCoords = getBoundingClientRec();
+  console.log(dropdownCoords);
 }
 
-function handleLeave(){
-    this.classList.remove('trigger-enter', 'trigger-enter-active');
-    background.classList.remove('open');
-
+function handleLeave() {
+  this.classList.remove("trigger-enter", "trigger-enter-active");
+  background.classList.remove("open");
 }
 
-triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
-triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
+triggers.forEach((trigger) =>
+  trigger.addEventListener("mouseenter", handleEnter)
+);
+triggers.forEach((trigger) =>
+  trigger.addEventListener("mouseleave", handleLeave)
+);
