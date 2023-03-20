0ad8a7c39d6f488d7e3712f207896de7f6f1e115
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-19 22:37:39 -0300

    creates the function for handling the video speedRate

--- Video-speed-controller/script.js
+++ Video-speed-controller/script.js
@@ -2,3 +2,14 @@ const speed = document.querySelector('.speed');
 const bar = document.querySelector('.speed-bar');
 const video = document.querySelector('.flex');
 
+speed.addEventListener('mousemove', function(e){
+    const y = e.pageY - this.offsetTop;
+    const percent = y / this.offsetHeight;
+    min = 0.4;
+    max = 4;
+    const height = Math.round(percent * 100) + '%';
+    const playbackRate = percent * (max - min) + min;
+    bar.style.height = height;
+    bar.textContent = playbackRate.toFixed(2) + 'x';
+    video.playbackRate = playbackRate;
+});
