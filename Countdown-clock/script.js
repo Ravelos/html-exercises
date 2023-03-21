cd95273607a8e48caf035c840f678456752e3470
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-21 20:54:29 -0300

    Creates the function displayTimeLeft

--- Countdown-clock/script.js
+++ Countdown-clock/script.js
@@ -1,22 +1,28 @@
 let countdown;
+const timerDisplay = document.querySelector(".display__time-left");
 
-function timer(seconds){
-    const now =  Date.now();
-    const then = now + seconds * 1000;
-    displayTimeLeft(seconds); 
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
 
-    countdown = setInterval(()=>{
-        secondsLeft = Math.round((then - Date.now()) / 1000);
-        if(secondsLeft < 0){
-            clearInterval(countdown);
-            return;
-        }
-        displayTimeLeft(secondsLeft);
-    }, 1000)
+  countdown = setInterval(() => {
+    secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
 }
 
-function displayTimeLeft(seconds){
-    const minutes = Math.floor(seconds/60);
-    const remainderSeconds = seconds % 60;
-    console.log({minutes, remainderSeconds});
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? "0" : ""
+  }${remainderSeconds}`;
+  
+  timerDisplay.textContent = display;
 }
