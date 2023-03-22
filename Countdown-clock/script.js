279acb317946798f3d12b77554c9dcf4de5b2d97
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-21 21:12:13 -0300

    Creates the function displayEndTime

--- Countdown-clock/script.js
+++ Countdown-clock/script.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -23,6 +25,16 @@ function displayTimeLeft(seconds) {
   const display = `${minutes}:${
     remainderSeconds < 10 ? "0" : ""
   }${remainderSeconds}`;
-  
+  document.title = display;
   timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
 }
